04c5e5e8c71362375342683a5aa255fda5c24be4
Ray Vernagus <r.vernagus@gmail.com>
2014-12-09 12:16:27 -0500

    Added Trial class.

---
+++ src/NBenchmarker/NBenchmarker.Tests/TrialTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NBenchmarker.Tests
+{
+    [TestClass]
+    public class TrialTests
+    {
+        [TestMethod]
+        public void HasAName()
+        {
+            var trial = new Trial("A Trial");
+            Assert.AreEqual("A Trial", trial.Name);
+        }
+
+        [TestMethod]
+        public void NewTrialHasEmptyActions()
+        {
+            var trial = new Trial("A Trial");
+            Assert.IsNotNull(trial.SetUp);
+            Assert.IsNotNull(trial.NotTimedIteration);
+            Assert.IsNotNull(trial.TimedIteration);
+            Assert.IsNotNull(trial.TearDown);
+        }
+    }
+}
---
+++ src/NBenchmarker/NBenchmarker/Trial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NBenchmarker
+{
+    public class Trial
+    {
+        public Trial(string name)
+        {
+            this.Name = name;
+            this.SetUp = () => { };
+            this.NotTimedIteration = () => { };
+            this.TimedIteration = () => { };
+            this.TearDown = () => { };
+        }
+
+        public string Name { get; private set; }
+
+        public Action SetUp { get; set; }
+
+        public Action NotTimedIteration { get; set; }
+
+        public Action TimedIteration { get; set; }
+
+        public Action TearDown { get; set; }
+    }
+}
