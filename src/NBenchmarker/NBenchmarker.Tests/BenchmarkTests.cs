1f1b4c66f78ce06439071df48b9f0af2eb421159
Ray Vernagus <r.vernagus@gmail.com>
2014-12-14 11:09:17 -0500

    Add Benchmark class

---
+++ src/NBenchmarker/NBenchmarker.Tests/BenchmarkTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NBenchmarker.Tests
+{
+    [TestClass]
+    public class BenchmarkTests
+    {
+        [TestMethod]
+        public void RunBenchmarkExecutesIteration()
+        {
+            var wasCalled = false;
+            var trial = new Trial("");
+            trial.TimedIteration = () => wasCalled = true;
+            Benchmark.Run(trial);
+            Assert.IsTrue(wasCalled, "Expected TimedIteration to be called");
+        }
+    }
+}
---
+++ src/NBenchmarker/NBenchmarker/Benchmark.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NBenchmarker
+{
+    public static class Benchmark
+    {
+        public static void Run(Trial trial)
+        {
+            trial.TimedIteration();
+        }
+    }
+}
