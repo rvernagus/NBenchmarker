70a90e2b8300cb669e6ac833445f2fb35946e68f
Ray Vernagus <r.vernagus@gmail.com>
2014-12-18 22:08:35 -0500

    Designing the API in Ruby...

--- src/ruby-design/benchmarker.rb
+++ src/ruby-design/benchmarker.rb
@@ -1,12 +1,52 @@
-# The basic concept is to time a chunk of code
-def benchmark(&proc)
-  start_time = Time.now
-  proc.call
-  end_time = Time.now
-  start_time - end_time
+class Trial
+  def before_benchmark; end
+  def before_benchmark_loop; end
+  def benchmark_loop; end
+  def after_benchmark_loop; end
+  def after_benchmark; end
 end
 
-result = benchmark do
-  sleep 1
+class MyTrial < Trial
+  def before_benchmark
+    puts "before_benchmark"
+  end
+  def before_benchmark_loop
+    puts "  before_benchmark_loop"
+  end
+  def benchmark_loop
+    puts "    benchmark_loop"
+    sleep 1
+  end
+  def after_benchmark_loop
+    puts "  after_benchmark_loop"
+  end
+  def after_benchmark
+    puts "after_benchmark"
+  end
 end
+
+# The basic concept is to time a chunk of code
+class Benchmarker
+  def benchmark(trial)
+    trial.before_benchmark
+
+    result = 0
+    0.upto(9) do
+      trial.before_benchmark_loop
+      start_time = Time.now
+      trial.benchmark_loop
+      end_time = Time.now
+      result += start_time - end_time
+      trial.after_benchmark_loop
+    end
+
+    trial.after_benchmark
+
+    result
+  end
+end
+
+puts "----------------"
+benchmarker = Benchmarker.new
+result = benchmarker.benchmark(MyTrial.new)
 puts result
