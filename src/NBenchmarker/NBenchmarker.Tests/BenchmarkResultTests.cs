f1cb2159d3242101809b6783d8217d4cb42882eb
Ray Vernagus <r.vernagus@gmail.com>
2015-01-03 23:30:14 -0500

    Reorganized BenchmarkResultTests

--- src/NBenchmarker/NBenchmarker.Tests/BenchmarkResultTests.cs
+++ src/NBenchmarker/NBenchmarker.Tests/BenchmarkResultTests.cs
@@ -17,47 +17,63 @@ namespace NBenchmarker.Tests
             result = new BenchmarkResult(trial.Name);
         }
 
-        [TestMethod]
-        public void InitialStateStoresTrialName()
+        [TestClass]
+        public class TheConstructor : BenchmarkResultTests
         {
-            Assert.AreEqual("trial1", result.TrialName);
-            Assert.AreEqual(TimeSpan.Zero, result.ElapsedTime);
-            Assert.AreEqual(0, result.NumberOfIterations);
+            [TestMethod]
+            public void StoresTrialName()
+            {
+                Assert.AreEqual("trial1", result.TrialName);
+                Assert.AreEqual(TimeSpan.Zero, result.ElapsedTime);
+                Assert.AreEqual(0, result.NumberOfIterations);
+            }
         }
 
-        [TestMethod]
-        public void CalculatesIterationAverageDuration()
+        [TestClass]
+        public class TheIterationAverageDurationProperty : BenchmarkResultTests
         {
-            // 1 / 1
-            result.ElapsedTime = TimeSpan.FromTicks(1);
-            result.NumberOfIterations = 1;
-            Assert.AreEqual(TimeSpan.FromTicks(1), result.IterationAverageDuration);
+            [TestMethod]
+            public void CalculatesIterationAverageDuration()
+            {
+                // 1 / 1
+                result.ElapsedTime = TimeSpan.FromTicks(1);
+                result.NumberOfIterations = 1;
+                Assert.AreEqual(TimeSpan.FromTicks(1), result.IterationAverageDuration);
 
-            // 2 / 2
-            result.ElapsedTime = TimeSpan.FromTicks(2);
-            result.NumberOfIterations = 2;
-            Assert.AreEqual(TimeSpan.FromTicks(1), result.IterationAverageDuration);
+                // 2 / 2
+                result.ElapsedTime = TimeSpan.FromTicks(2);
+                result.NumberOfIterations = 2;
+                Assert.AreEqual(TimeSpan.FromTicks(1), result.IterationAverageDuration);
 
-            // 10000 / 50
-            result.ElapsedTime = TimeSpan.FromTicks(10000);
-            result.NumberOfIterations = 50;
-            Assert.AreEqual(TimeSpan.FromTicks(200), result.IterationAverageDuration);
+                // 10000 / 50
+                result.ElapsedTime = TimeSpan.FromTicks(10000);
+                result.NumberOfIterations = 50;
+                Assert.AreEqual(TimeSpan.FromTicks(200), result.IterationAverageDuration);
+            }
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        [ExcludeFromCodeCoverage] // Shows as Not Covered even though it is executed
-        public void NegativeElapsedTimeCausesException()
+        [TestClass]
+        public class TheElapsedTimeProperty : BenchmarkResultTests
         {
-            result.ElapsedTime = TimeSpan.FromTicks(-1);
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            [ExcludeFromCodeCoverage] // Shows as Not Covered even though it is executed
+            public void ThrowsAnExceptionWhenGivenANegativeTime()
+            {
+                result.ElapsedTime = TimeSpan.FromTicks(-1);
+            }
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        [ExcludeFromCodeCoverage] // Shows as Not Covered even though it is executed
-        public void NegativeIterationsCausesException()
+        [TestClass]
+        public class TheNumberOfIterationsProperty : BenchmarkResultTests
         {
-            result.NumberOfIterations = -1;
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            [ExcludeFromCodeCoverage] // Shows as Not Covered even though it is executed
+            public void ThrowsAnExceptionWhenGivenANegativeNumberOfIterations()
+            {
+                result.NumberOfIterations = -1;
+            }
         }
     }
 }
